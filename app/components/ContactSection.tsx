3c1d76bf15d6b5f9eaab8bbb1c121f5b30131bcf
Nick McBride <namcbrid@iu.edu>
2025-08-22 16:52:03 -0400

    adding some things

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -13,7 +13,7 @@ export default function ContactSection() {
 					transition={{ duration: 0.6 }}
 					className="text-3xl md:text-4xl font-bold mb-8"
 				>
-					Let&apos;s Connect
+					Contact Me
 				</motion.h2>
 				<motion.p
 					initial={{ opacity: 0, y: 20 }}
@@ -22,7 +22,7 @@ export default function ContactSection() {
 					transition={{ duration: 0.6, delay: 0.2 }}
 					className="text-gray-300 mb-6"
 				>
-					Have a project in mind? Let&apos;s create something amazing together.
+					Let&apos;s chat about potential opportunities.
 				</motion.p>
 
 				<motion.div
@@ -35,7 +35,7 @@ export default function ContactSection() {
 					<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
 						<path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
 					</svg>
-					<span>San Francisco, CA</span>
+					<span>Bloomington, IN</span>
 				</motion.div>
 
 				<motion.div
@@ -84,11 +84,6 @@ export default function ContactSection() {
 					<a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="p-2 bg-white/5 rounded-full hover:bg-white/10 transition-colors">
 						<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
 							<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-						</svg>
-					</a>
-					<a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="p-2 bg-white/5 rounded-full hover:bg-white/10 transition-colors">
-						<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-							<path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
 						</svg>
 					</a>
 				</motion.div>
