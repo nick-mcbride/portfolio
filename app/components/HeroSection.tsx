b27fbb0e4902df649446a0e70fdd3c372088181c
Nick McBride <namcbrid@iu.edu>
2026-07-15 12:40:24 -0400

    fixing hero and contact sections

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -30,7 +30,7 @@ export default function HeroSection() {
 					transition={{ duration: 0.8, delay: 0.4 }}
 					className="text-lg md:text-xl text-gray-300 mb-8 px-4"
 				>
-					Computer Science Student & Developer
+					Computer Scientist & Software Engineer
 				</motion.p>
 			</div>
 
